0121215ba16a015a2db1652b30c3fecb2e7a1ac1
testing.rahul@gmail.com <testing.rahul@gmail.com>
2025-11-23 18:42:53 -0800

    updated pom

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,15 +2,14 @@ package base;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 import utils.ExtentManager;
 
@@ -33,9 +32,27 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup(Method method) {
+
         test = extent.createTest(method.getName());
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+
+        // Install correct ChromeDriver automatically
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Detect if running in CI (GitHub Actions)
+        boolean isCI = "true".equalsIgnoreCase(System.getenv("CI"));
+
+        if (isCI) {
+            // Required flags for Linux CI environment
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
